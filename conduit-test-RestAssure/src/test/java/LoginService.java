e2c51c7ce2d518b68755e93e90962edf6d2e1aa6
Camilo <jcam.qvision@gmail.com>
2022-01-25 10:24:34 -0500

    RestAssure login service - token

---
+++ conduit-test-RestAssure/src/test/java/LoginService.java
@@ -0,0 +1,52 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LoginService {
+    
+    static String token;
+
+    @Before
+    public void setUp(){
+        baseURI = "https://api.realworld.io";
+        basePath = "/api";
+        filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
+        requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    @Test
+    public void login(){
+         token =
+                given()
+                        .when()
+                        .body("{\n" +
+                                "  \"user\": {\n" +
+                                "    \"email\": \"jcam.qvision@gmail.com\",\n" +
+                                "    \"password\": \"testerExtremo\"\n" +
+                                "  }\n" +
+                                "}")
+                        .post("/users/login")
+                        .then()
+                        .statusCode(HttpStatus.SC_OK)
+                        .body("user.username", equalTo("Donatelo"))
+                        .extract()
+                        .jsonPath().getString("user.token");
+
+        System.out.println(token);
+
+
+
+
+    }
+}
