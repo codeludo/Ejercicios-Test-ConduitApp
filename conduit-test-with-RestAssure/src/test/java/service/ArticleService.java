59ba4f03cb5d2aa84673f9f99e22ee7dca4a306c
Camilo <jcam.qvision@gmail.com>
2022-01-27 20:27:20 -0500

    article service - post random article

--- conduit-test-with-RestAssure/src/test/java/service/ArticleService.java
+++ conduit-test-with-RestAssure/src/test/java/service/ArticleService.java
@@ -1,4 +1,48 @@
 package service;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utils.RandomArticle;
+import utils.Token;
+
+import static io.restassured.RestAssured.*;
+
 public class ArticleService {
+    public int articlesCount;
+
+    @BeforeClass
+    public static void setUp(){
+        baseURI = "https://api.realworld.io";
+        basePath = "/api";
+        filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
+        requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
+        responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+    }
+
+    
+    @Test
+    public void postNewRandomArticle(){
+        Token.setToken();
+        RandomArticle.setArticle();
+
+        given().
+                spec(requestSpecification).
+                header("authorization", "Bearer "+ Token.getToken()).
+                body(RandomArticle.getArticle()).
+                when().
+                post("/articles").
+                then().
+                spec(responseSpecification);
+    }
 }
--- conduit-test-with-RestAssure/src/test/java/utils/RandomArticle.java
+++ conduit-test-with-RestAssure/src/test/java/utils/RandomArticle.java
@@ -1,4 +1,74 @@
 package utils;
 
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RandomArticle {
+    private static List<String> tags;
+    private static String title;
+    private static String description;
+    private static String body;
+    static Map<String, Object>  article = new HashMap<>();
+
+    static Faker faker = new Faker();
+
+
+    public static List<String> getTags() {
+        return tags;
+    }
+
+    private static void setTags() {
+        ArrayList<String> tags = new ArrayList<String>();
+        tags.add(faker.lorem().word());
+        RandomArticle.tags = tags;
+    }
+
+    public static String getTitle() {
+        return title;
+    }
+
+
+    private static void setTitle() {
+        RandomArticle.title = faker.lorem().sentence(1, 2);
+    }
+
+    public static String getDescription() {
+        return description;
+    }
+
+    private static void setDescription() {
+        RandomArticle.description = faker.lorem().sentence(3);;
+    }
+
+    public static String getBody() {
+        return body;
+    }
+
+    private static void setBody() {
+        RandomArticle.body = faker.lorem().paragraph(5);
+    }
+
+    public static void setArticle() {
+        setTags();
+        setTitle();
+        setDescription();
+        setBody();
+
+        article.put("tags", getTags());
+        article.put("title", getTitle());
+        article.put("description", getDescription());
+        article.put("body", getBody());
+
+
+    }
+
+    public static Map<String, Object> getArticle() {
+        Map<String, Object>  jsonObject = new HashMap<>();
+        jsonObject.put("article", article);
+        return jsonObject;
+    }
 }
